49f376a804c965a4156d9b8c10e18209388d1a18
nelioasousa <corneliosousaphb@gmail.com>
2026-04-25 11:10:46 -0300

    Experiment 01

---
+++ experiments/exp_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import csv
+
+DATASET = Path("datasense/dataset")
+
+print("EXP 01 : DATASET SAMPLES AND TOTAL SIZE", end="\n\n")
+for tp in ("attack", "benign"):
+    print(f"=== {tp.upper()} DATA SAMPLE ===")
+    data_path = DATASET / f"{tp}_samples_1sec.csv"
+    with data_path.open("r") as f:
+        csv_data = csv.reader(f)
+        columns = next(csv_data)
+        padding = max(len(c) for c in columns)
+        for entry_idx, entry in enumerate(csv_data):
+            if entry_idx == 30000:
+                for colm, data in zip(columns, entry):
+                    print(f">>> {colm.rjust(padding, "_")} : {data}")
+    print(f"\nSIZE: {entry_idx + 1}", end=("\n\n\n" if tp == "attack" else "\n"))
