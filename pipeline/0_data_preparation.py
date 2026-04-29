a6d50682494049913d7f61c44cfdc0a22aa57749
nelioasousa <corneliosousaphb@gmail.com>
2026-04-29 13:52:55 -0300

    data_preparation step coding

--- pipeline/0_data_preparation.py
+++ pipeline/0_data_preparation.py
@@ -1,11 +1,18 @@
 import csv
 from pathlib import Path
+from ast import literal_eval
 import pandas as pd
 from collections.abc import Sequence
 
 
 RAW_BENIGN_DATA_PATH = Path("datasense/dataset/benign_samples_1sec.csv")
 RAW_ATTACK_DATA_PATH = Path("datasense/dataset/attack_samples_1sec.csv")
+CONVERTERS = {
+    "network_protocols_dst": literal_eval,
+    "network_protocols_src": literal_eval,
+    "network_ports_dst": literal_eval,
+    "network_ports_src": literal_eval,
+}
 DROP_COLUMNS = frozenset([
     "device_mac", "timestamp", "timestamp_start", "timestamp_end",
     "log_data-ranges_avg", "log_data-ranges_max", "log_data-ranges_min",
@@ -16,9 +23,22 @@ DROP_COLUMNS = frozenset([
 ])
 
 
-IIOT_KEEP_PROTOCOLS = frozenset(["mqtt", "data"])
+DOS_DDOS_PORTS = frozenset([
+    "22", "23", "80", "443", "554",
+    "557", "1883", "6668", "8000", "9595",
+])
+
+IOT_KEEP_PROTOCOLS = frozenset(["mqtt", "data"])
+IOT_KEEP_PORTS = frozenset([
+    "80", "443", "1883", "8883", "5683",
+    "5684", "5671", "5672", "5353", "1900",
+])
+
 BROKER_KEEP_PROTOCOLS = frozenset(["mqtt"])
+BROKER_KEEP_PORTS = frozenset([])
+
 GENERAL_KEEP_PROTOCOLS = 40
+GENERAL_KEEP_PORTS = frozenset([])
 
 
 def get_csv_columns(csv_path: Path) -> list[str]:
@@ -40,8 +60,9 @@ def get_csv_columns(csv_path: Path) -> list[str]:
 def prepare_datasets():
     target_columns = get_csv_columns(RAW_BENIGN_DATA_PATH)
     target_columns = [c for c in target_columns if c not in DROP_COLUMNS]
-    benign_data = pd.read_csv(RAW_BENIGN_DATA_PATH, usecols=target_columns)
-    attack_data = pd.read_csv(RAW_ATTACK_DATA_PATH, usecols=target_columns)
+    benign_data = pd.read_csv(RAW_BENIGN_DATA_PATH, usecols=target_columns, converters=CONVERTERS)
+    attack_data = pd.read_csv(RAW_ATTACK_DATA_PATH, usecols=target_columns, converters=CONVERTERS)
+    
     return
 
 
